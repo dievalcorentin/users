e22d252f1cabd67c8672a2b644ec0fdfa7c37ac9
Corentin Dieval <corentin@graphmytech.fr>
2023-12-18 15:51:07 +0100

    fix:

--- AdministrationUsers/src/main/java/com/graphmytech/administrationusers/AdministrationUsersApplication.java
+++ AdministrationUsers/src/main/java/com/graphmytech/administrationusers/AdministrationUsersApplication.java
@@ -2,10 +2,8 @@ package com.graphmytech.administrationusers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.graphmytech.AdministrationUsers", "com.graphmytech.AdministrationUsers.Controller", "com.graphmytech.AdministrationUsers.Repository"})
 public class AdministrationUsersApplication {
 
 	public static void main(String[] args) {
