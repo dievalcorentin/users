d511bca8df96ea2da8a596a78ed389e05ccf7c5f
Corentin Dieval <corentin@graphmytech.fr>
2024-01-02 10:37:54 +0100

    feature: changes made by Thomas

--- AdministrationUsers/src/main/java/com/graphmytech/administrationusers/controller/UserController.java
+++ AdministrationUsers/src/main/java/com/graphmytech/administrationusers/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.graphmytech.administrationusers.controller;
 
+import com.graphmytech.administrationusers.dto.CreateUserDTO;
 import com.graphmytech.administrationusers.dto.UserDTO;
 import com.graphmytech.administrationusers.service.UserService;
+import com.graphmytech.administrationusers.mapper.UserMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,35 +29,35 @@ public class UserController {
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
-    // Endpoint to get details of a user by ID
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
-        UserDTO user = userService.getUserById(id);
-        return user != null ?
-                new ResponseEntity<>(user, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    // Endpoint to update user details
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDetails) {
-        UserDTO updatedUser = userService.updateUser(id, userDetails);
-        return updatedUser != null ?
-                new ResponseEntity<>(updatedUser, HttpStatus.OK) :
-                new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    // Endpoint to delete a user by ID
-    @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+//
+//    // Endpoint to get details of a user by ID
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+//        UserDTO user = userService.getUserById(id);
+//        return user != null ?
+//                new ResponseEntity<>(user, HttpStatus.OK) :
+//                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+//
+//    // Endpoint to update user details
+//    @PutMapping("/{id}")
+//    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDetails) {
+//        UserDTO updatedUser = userService.updateUser(id, userDetails);
+//        return updatedUser != null ?
+//                new ResponseEntity<>(updatedUser, HttpStatus.OK) :
+//                new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//    }
+//
+//    // Endpoint to delete a user by ID
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
+//        userService.deleteUser(id);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+//
 
     // Endpoint to get all users
-    @GetMapping("/all")
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        List<UserDTO> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
+
+
 }
